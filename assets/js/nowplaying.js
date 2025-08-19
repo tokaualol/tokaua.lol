ed6bc43bc0645376ff2db65e69eeb10ccf21a51c
tokaua <tokaua@protonmail.com>
2025-08-18 23:37:19 -0700

    song display on socials page, moved gohome to nav

---
+++ assets/js/nowplaying.js
@@ -0,0 +1,37 @@
+const USERNAME = "tokaua";
+const BASE_URL = `https://lastfm-last-played.biancarosa.com.br/${USERNAME}/latest-song`;
+
+const getTrack = async () => {
+    const request = await fetch(BASE_URL);
+    const json = await request.json();
+    let status
+
+    let currentlyplaying
+
+    let isPlaying = json.track['@attr']?.nowplaying || false;
+
+    if(!isPlaying) {
+        
+        document.getElementById("listening").innerHTML = `
+        <a href="https://last.fm/user/tokaua">Last Played</a>
+        <img src="${json.track.image[2]['#text']}">
+        <div>
+        <p>${json.track.name}<br>${json.track.artist['#text']}</p>
+        </div>
+        `
+        return;
+    } else {
+        currentlyplaying = "Now Playing"
+    }
+
+    document.getElementById("listening").innerHTML = `
+    <a href="https://last.fm/user/tokaua">${currentlyplaying}</a>
+    <img src="${json.track.image[2]['#text']}" id="coverart">
+    <div>
+    <a>${json.track.name}<br>by ${json.track.artist['#text']}</a>
+    </div>
+    `
+};
+
+getTrack();
+setInterval(() => { getTrack(); }, 30000);
